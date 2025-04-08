390dd38a33bb6e8978de50f96424a3d4b47675ad
Ilia <ilia_shcherbakov1234@mail.ru>
2025-04-08 20:22:19 +0700

    Add code for begin32

---
+++ begin32.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class begin32
+{
+    static void Main()
+    {
+        double t, tf;
+        Console.WriteLine("Введите температуру по Цельсию: ");
+        t = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine(t);
+        tf = t * (9.0 / 5.0) + 32;
+        Console.WriteLine("Ответ: {0}", tf);
+    }
+}
