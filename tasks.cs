13889297151ba8b713d4feccfb0871793cf36df1
Ilia <ilia_shcherbakov1234@mail.ru>
2025-04-09 09:50:56 +0700

    Add code for while4

--- tasks.cs
+++ tasks.cs
@@ -4,16 +4,11 @@ class Tasks
 {
     static void Main()
     {
-        int a, b;
-        int arg = 1;
-        Console.Write("Введите A: ");
-        a = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Введите B: ");
-        b = Convert.ToInt32(Console.ReadLine());
-        for (int i = a;  a <= b; a ++)
-        {
-            arg *= a;
-        }
-        Console.WriteLine("{0}", arg);
+        int value, power = 1;
+        Console.Write("Введите число: ");
+        value = Convert.ToInt32(Console.ReadLine());
+        while (power < value) { power *= 3; }
+        if (value == power) { Console.WriteLine("Истина"); }
+        else { Console.WriteLine("Ложь"); }
     }
 }
