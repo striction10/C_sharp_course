8839a352a303dc77d52ad1ba5983b33920e5db8d
Ilia <ilia_shcherbakov1234@mail.ru>
2025-04-09 13:30:56 +0700

    Add tests integer11

--- tasks.cs
+++ tasks.cs
@@ -1,19 +1,43 @@
 ﻿using System;
 
-public class begin32
+public class Integer11
 {
     public static void Main()
     {
-        double t;
-        Console.WriteLine("Введите температуру по Цельсию: ");
-        t = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine(t);
-        Console.WriteLine("Ответ: {0:F2}", Change(t));
+        int number, sum, arg, num_1, num_2, num_3;
+        Console.WriteLine("Введите трехзначное число: ");
+        number = Convert.ToInt32(Console.ReadLine());
+        num_3 = Num3(number);
+        num_2 = Num2(number);
+        num_1 = Num1(number);
+        sum = Sum(num_3, num_2, num_1);
+        arg = Arg(num_3, num_2, num_1);
+        Console.WriteLine("Получились числа: {0}, {1}, {2}", num_1, num_2, num_3);
+        Console.WriteLine("Сумма равна: {0}", sum);
+        Console.WriteLine("Произведение равно: {0}", arg);
     }
-    public static double Change(double t)
+    public static int Num3(int num) 
+    { 
+        return num % 10; 
+    }
+    public static int Num2(int num)
     {
-        double tf;
-        tf = t * (9.0 / 5.0) + 32;
-        return tf;
+        return num % 100 / 10;
+    }
+    public static int Num1(int num)
+    {
+        return num / 100;
+    }
+    public static int Sum(int num_3,  int num_2, int num_1)
+    {
+        int sum;
+        sum = num_3 + num_2 + num_1;
+        return sum;
+    }
+    public static int Arg(int num_3, int num_2, int num_1)
+    {
+        int arg;
+        arg = num_3 * num_2 * num_1;
+        return arg;
     }
 }
